f98510bf011d4e2b08cc2adca8acf126cf6d6ba2
huazhilun <nigulasikaochuan@sjtu.edu.cn>
2019-06-27 14:59:54 +0800

    variable

---
+++ src/Variable.java
@@ -0,0 +1,34 @@
+public class Variable {
+    public static  void main_variable(String[]args){
+       /*
+       * 1.变量：
+       *    原生变量：
+       *        byte short int long
+       *        double float
+       *        char
+       *        boolean
+       *   引用变量
+       *
+       *   自动转换：
+       *        char short byte 在参与运算时候，会自动
+       *        转换成int
+       *
+       *        数字+字符串 转变成字符串
+       *
+       *        自动类型转换：
+       *            char---> int--->long--->float--->double
+       *            byte--->short--->int--->long--->float--->double
+       *        表达式的类型一定和最高的类型相同：
+       *        short a = 4;
+       *        a+3; 是int 类型
+       *
+       *        'a'+4 是int 类型
+       *
+       *        4+4.2 是double类型
+       *
+       *        4+4.2f 是float 类型.
+       * */
+
+    }
+
+}
